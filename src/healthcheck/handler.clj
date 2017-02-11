f853c5a96aa1de3a8bc0f32833d1581e1fda655c
Alex Fillis <alexfillis@gmail.com>
2017-02-11 18:27:43 +0800

    #2 Added some stubbed checks (dont actually really check anything) that are called when you hit that status endpoint.

--- src/healthcheck/handler.clj
+++ src/healthcheck/handler.clj
@@ -1,11 +1,37 @@
 (ns healthcheck.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]))
+
+(defn path-check [path]
+  (fn []
+    {:result :ok}))
+
+(defn url-check [url]
+  (fn []
+    {:result :ok}))
+
+(defn health-check [key f]
+  (fn []
+    {key (f)}))
+
+(def hc [(health-check "Temporary directory exists?" 
+                       (path-check "/tmp"))
+         (health-check "Example.com is available?" 
+                       (url-check "http://www.example.com"))])
+
+(defn check-health [checks]
+  (reduce (fn [coll f] (conj coll (f))) [] checks))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/status" [] (response (check-health hc)))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
+
